f5c70d82f066f80096d84cecb473431476d01302
Asish Bhandari <asish.bhandari17@gmail.com>
2023-09-18 21:59:17 +0530

    correction for path

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ import {createPost} from './controllers/userPostRoutes/posts.js';
 import { verifyToken } from './middleware/verifyAuth.js';
 
 //  configurations
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app= express();
 app.use(express.json());
